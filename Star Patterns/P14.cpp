3ceb0d67baa1cc561af690f12d48bd6dd9a227b6
Sumit Singh <shinghsumit01@gmail.com>
2026-01-28 14:02:40 +0530

    Add files via upload

---
+++ Star Patterns/P14.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n=5;
+    for(int i=1;i<=n;i++){
+        for (int s=1; s<=n-i; s++){
+            cout<<"  ";
+        }
+        for(int j=1;j<=i;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    for(int i=1;i<=n-1;i++){
+        for (int s=1; s<=i; s++){
+            cout<<"  ";
+        }
+        for(int j=1;j<=n-i;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
