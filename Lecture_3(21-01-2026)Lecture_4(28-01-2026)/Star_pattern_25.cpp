445eb4ea448eaeff10c08825b9b4af3b0a29d478
Sumit Singh <shinghsumit01@gmail.com>
2026-04-13 22:41:46 +0530

    Add files via upload

---
+++ Lecture_3(21-01-2026)Lecture_4(28-01-2026)/Star_pattern_25.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int n = 5;
+    int num = 1;
+
+    for(int i = 1; i <= n; i++) 
+    {
+        for(int s = 1; s <= n - i; s++) 
+        {
+            cout << "     ";
+        }
+
+        for(int j = 1; j <= 2*i - 1; j++) 
+        {
+            cout <<" "<< num << "   ";
+            num++;
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
